f9b461110b06dd64a8da068215b8d09147dca803
ibrahimmt11 <127002385+ibrahimmt11@users.noreply.github.com>
2023-03-08 16:50:15 +0700

    menambahkan implementasi kalkulator

--- modul3_1302210022/Form1.cs
+++ modul3_1302210022/Form1.cs
@@ -12,19 +12,29 @@ namespace modul3_1302210022
 {
     public partial class Form1 : Form
     {
+        int digit1;
         public Form1()
         {
             InitializeComponent();
         }
 
+
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if(textBox1 == null)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-
+            digit1 = int.Parse(textBox1.Text);
+            textBox1.Text = "+";
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -39,7 +49,14 @@ namespace modul3_1302210022
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            if (textBox1 == null)
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -49,12 +66,105 @@ namespace modul3_1302210022
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            if (textBox1 == null)
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
+            if (textBox1 == null)
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1 == null)
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int hasil;
+            hasil = digit1 + int.Parse(textBox1.Text);
+            textBox1.Text = hasil.ToString();
         }
     }
 }
